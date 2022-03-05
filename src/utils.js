0cd325d9e5d3efeaa4d033cee51e589d4df96c26
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-05 16:12:08 -0500

    Write pagination logic.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 10;
+  const pages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  //create new arrays of arrrays (undefined at first)
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage; //grab starting point of each array
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+  return newFollowers;
+};
+export default paginate;
