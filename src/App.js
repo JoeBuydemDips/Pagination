67c4e86ad129f77bd5a01139451885e039e27d59
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-05 17:10:26 -0500

    Add prev and next buttons and their functionality.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,26 @@ function App() {
     setFollowers(data[page]);
   }, [loading, page]);
 
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
   return (
     <main>
       <div className="section-title">
@@ -25,6 +45,9 @@ function App() {
         </div>
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
             {data.map((_, index) => {
               return (
                 <button
@@ -36,6 +59,9 @@ function App() {
                 </button>
               );
             })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </section>
